e1ff65296d6253b47f9a78b2525f132494dc27c8
GiannisAUEB <giannis9700@gmail.com>
2021-05-01 20:05:11 +0300

    started main
    all my homies hate c

--- p3150011-p3170065-pizza.c
+++ p3150011-p3170065-pizza.c
@@ -1,3 +1,52 @@
+#include "p3150011-p3170065-pizza.h"
+
+int i, n_cust, code;
+unsigned int seed;
+pthread_mutex_t lock;
+pthread_mutex_t mutex_rand_r = PTHREAD_MUTEX_INITIALIZER;
+
+void check_mutex(int code)
+{
+	if (code != 0)
+	{
+		printf("Error in mutex lock/unlock! Error Code: %d \n", code);
+		pthread_exit(&code);
+	}
+}
+
 int main(int argc, char *argv[]) {
-	return 1;
+	int code;
+	if (argc < 2) {
+		printf("Need at least two arguments.\n");
+		printf("Exiting...");
+		exit(-1);
+	}
+
+	if(atoi(argv[1])>0){
+		n_cust = atoi(argv[1]);
+	}else{
+		printf("Need at least 1 customer");
+		exit(-1);
+	}
+
+	seed = atoi(argv[2]);
+	int cust_id[n_cust];
+	
+	pthread_t thread[n_cust];
+
+	for(i = 0; i<n_cust; i++) {
+		cust_id[i] = i + 1;
+		pthread_create(&thread[i], NULL, &pizza_thread, &cust_id[i]);
+		
+		code = pthread_mutex_lock(&mutex_rand_r);
+		check_mutex(code);
+		
+		sleep(rand_r(&seed) % time_order_h + time_order_l);
+	}
+	
+	return 0;
+}
+
+void * pizza_thread(void *cust_id) {
+	pthread_exit(NULL);
 }
--- p3150011-p3170065-pizza.h
+++ p3150011-p3170065-pizza.h
@@ -4,27 +4,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-const int N_tel = 3;            // Number of call handlers
-const int N_cook = 2;           // Number of cooks
-const int N_oven = 10;          // Number of ovens
-const int N_deliveres = 7;      // Number of deliverers
+const int n_tel = 3;            // Number of call handlers
+const int n_cook = 2;           // Number of cooks
+const int n_oven = 10;          // Number of ovens
+const int n_del = 7;      		// Number of deliverers
 
-const int T_order_low = 1*60;      // Time range in which a new order arrives
-const int T_order_high = 5*60;
+const int t_order_l = 1;      	// Time range in which a new order arrives
+const int t_order_h = 5;
 
-const int N_order_low = 1;      // Amount range of pizzas ordered
-const int N_order_high = 5;
+const int n_order_l = 1;      	// Amount range of pizzas ordered
+const int n_order_h = 5;
 
-const int T_payment_low = 1*60;    // Time range in which the payment is completed
-const int T_payment_high = 2*60;
+const int t_pay_l = 1;    		// Time range in which the payment is completed
+const int t_pay_h = 2;
 
-const int P_fail = 0.05;        // Probability that payment fails
+const int p_fail = 0.05;        // Probability that payment fails
 
-const int C_pizza = 10;         // Pizza cost in Euros
+const int c_pizza = 10;         // Pizza cost in Euros
 
-const int T_prep = 1*60;           // Time required to prepare each pizza
-const int T_bake = 10*60;          // Time required to bake each pizza
-const int T_pack = 2*60;           // Time required to pack each pizza
+const int t_prep = 1;           // Time required to prepare each pizza
+const int t_bake = 10;          // Time required to bake each pizza
+const int t_pack = 2;           // Time required to pack each pizza
 
-const int T_delivery_low = 5*60;   // Time range that deliveries take
-const int T_delivery_low = 15*60;
+const int t_del_l = 5;   		// Time range that deliveries take
+const int t_del_h = 15;
+
+void check_mutex(int code);
+void * pizza_thread(void * cust_id);
